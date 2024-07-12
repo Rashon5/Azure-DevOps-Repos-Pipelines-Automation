f1432a3ef8a0d5e8181dcda1b69b0d9cb67a3561
Rashon5 <124827377+Rashon5@users.noreply.github.com>
2024-07-11 22:32:35 -0400

    Update Destroy-Storage-Accounts.sh

--- Destroy-Storage-Accounts.sh
+++ Destroy-Storage-Accounts.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Define the resource group containing the storage accounts
+RESOURCE_GROUP="<your-resource-group>"
+
 # Delete storage accounts
-az storage account delete --name jwtst --resource-group azurebootcamp --yes
-az storage account delete --name jwqa --resource-group azurebootcamp --yes
-az storage account delete --name jwprd --resource-group azurebootcamp --yes
+az storage account delete --name jwtst --resource-group $RESOURCE_GROUP --yes
+az storage account delete --name jwqa --resource-group $RESOURCE_GROUP --yes
+az storage account delete --name jwprd --resource-group $RESOURCE_GROUP --yes
 
 echo "Storage accounts jwtst, jwqa, and jwprd have been deleted."
